f94aac80e9cd222fa65c86ffd12a2b1f3a028977
sremedios <sam.remedios@gmail.com>
2018-12-01 14:28:23 -0600

    finished part 2

--- src/bin/day_01.rs
+++ src/bin/day_01.rs
@@ -1,15 +1,20 @@
+use std::collections::HashSet;
 use std::env;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, ErrorKind};
+use std::io::{self, BufRead, BufReader, ErrorKind, Read, Seek, SeekFrom};
 
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let f = File::open(filename)?;
 
+    let mut reader = BufReader::new(&f);
+
+    // Part One
     println!(
-        "{}",
-        BufReader::new(f)
+        "Part one solution: {}",
+        reader
+            .by_ref()
             .lines()
             .map(|l| l?.parse::<i32>().map_err(|err| io::Error::new(
                 ErrorKind::Other,
@@ -17,6 +22,35 @@ fn main() -> Result<(), io::Error> {
             ))).flatten()
             .sum::<i32>()
     );
+
+    // Part Two
+    reader.seek(SeekFrom::Start(0))?;
+
+    let mut frequencies = HashSet::new();
+    let mut frequency: i32 = 0;
+
+    let drifts: Vec<i32> = reader
+        .by_ref()
+        .lines()
+        .map(|l| {
+            l?.parse::<i32>().map_err(|err| {
+                io::Error::new(ErrorKind::Other, format!("An error occurred: {}", err))
+            })
+        }).flatten()
+        .collect();
+
+    let mut cycle = drifts.iter().cycle();
+
+    let answer = loop {
+        frequency += cycle.next().unwrap();
+
+        if frequencies.contains(&frequency) {
+            break frequency;
+        } else {
+            frequencies.insert(frequency);
+        }
+    };
+    println!("Part two solution: {}", answer);
 
     Ok(())
 }
