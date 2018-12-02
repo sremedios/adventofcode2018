77848e162a07bc78c5ea04c6299bbf1626701a71
sremedios <sam.remedios@gmail.com>
2018-12-01 23:58:03 -0500

    refactor to manage all days

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,8 +1,60 @@
 use std::collections::HashSet;
-use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, ErrorKind, Read, Seek, SeekFrom};
 
+pub fn part_1(filename: &str) -> Result<i32, io::Error> {
+    let f = File::open(filename)?;
+    let answer = BufReader::new(&f)
+        .by_ref()
+        .lines()
+        .map(|l| {
+            l?.parse::<i32>().map_err(|err| {
+                io::Error::new(ErrorKind::Other, format!("An error occurred: {}", err))
+            })
+        }).flatten()
+        .sum::<i32>();
+    Ok(answer)
+}
+
+pub fn part_2(filename: &str) -> Result<i32, io::Error> {
+    let f = File::open(filename)?;
+
+    let mut frequencies = HashSet::new();
+    let mut frequency: i32 = 0;
+
+    let mut reader = BufReader::new(&f);
+
+    let answer = loop {
+        frequency += match reader.by_ref().lines().next().map(|x| {
+            x?.parse::<i32>().map_err(|err| {
+                io::Error::new(ErrorKind::Other, format!("An error occurred: {}", err))
+            })
+        }) {
+            Some(n) => n?,
+            None => {
+                reader.seek(SeekFrom::Start(0))?;
+                match reader.by_ref().lines().next().map(|x| {
+                    x?.parse::<i32>().map_err(|err| {
+                        io::Error::new(ErrorKind::Other, format!("An error occurred: {}", err))
+                    })
+                }) {
+                    Some(n) => n?,
+                    None => 0i32,
+                }
+            }
+        };
+
+        if frequencies.contains(&frequency) {
+            break frequency;
+        } else {
+            frequencies.insert(frequency);
+        }
+    };
+
+    Ok(answer)
+}
+
+/*
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
@@ -60,3 +112,4 @@ fn main() -> Result<(), io::Error> {
 
     Ok(())
 }
+*/
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::io;
+
+mod day_01;
+
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    let (part_1_answer, part_2_answer) = match args[1].as_ref() {
+        "1" => (day_01::part_1(&args[2])?, day_01::part_2(&args[2])?),
+        _ => (0, 0),
+    };
+
+    println!(
+        "Day {:?}\nPart 1: {:?}\nPart 2: {:?}",
+        args[2], part_1_answer, part_2_answer
+    );
+
+    Ok(())
+}
